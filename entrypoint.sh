e145ef1f33771071df450e2f1a8bb9f95840d716
Chris H. Meyer <christian.h.meyer@t-online.de>
2023-06-06 23:32:54 +0200

    Build release version

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ set -u
 git clone https://github.com/zynaddsubfx/zyn-fusion-build zyn-fusion-build
 cd zyn-fusion-build
 
-make -f Makefile.linux.mk install_deps
+make MODE=release -f Makefile.linux.mk install_deps
 PARALLEL=1 make -f Makefile.linux.mk all
