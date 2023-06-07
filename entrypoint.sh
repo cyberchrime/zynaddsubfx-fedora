2021830eb220fed350cc1518041b6be47bec944c
Chris H. Meyer <christian.h.meyer@t-online.de>
2023-06-07 08:31:30 +0200

    Ignore output directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ set -u
 git clone https://github.com/zynaddsubfx/zyn-fusion-build zyn-fusion-build
 cd zyn-fusion-build
 
-make MODE=release -f Makefile.linux.mk install_deps
-PARALLEL=1 make -f Makefile.linux.mk all
+make -f Makefile.linux.mk install_deps
+PARALLEL=1 make MODE=release -f Makefile.linux.mk all
 
 cp build/*.bz2 /output/
